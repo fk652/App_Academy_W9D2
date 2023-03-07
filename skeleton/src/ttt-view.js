4d0555579e051b0a9e3b81177883dc001efe92cc
Fahim Khan <fk652@nyu.edu>
2023-03-07 16:17:21 -0500

    update handleClick

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -13,6 +13,7 @@ class View {
     ul.style.flexWrap = "wrap";
     ul.style.listStyleType = 'none';
     ul.style.margin = '30px auto';
+    const inactiveColor = '#e0e0e0';
 
     for (let i = 0; i < 3; i++){
       for (let j = 0; j < 3; j++){
@@ -20,46 +21,75 @@ class View {
         li.style.width = '90px';
         li.style.height = '90px';
         li.style.border = '1px solid black';
-        const inactiveColor = '#e0e0e0';
         li.style.background = inactiveColor;
         li.setAttribute('data-pos',`[${i},${j}]`);
-        li.addEventListener(
-          'mouseover',
-          (event) => {
-            //console.log(event.target.className);
-            if (event.target.className === ''){
-              event.target.style.background = 'yellow';
-            } 
-            // setTimeout(() => {
-            //   event.target.style.background = inactiveColor;
-            // },
-            // 500
-            // )
-          }
-        );
-
-        li.addEventListener(
-          'mouseout',
-          (event) => {
-            if (event.target.className === ''){
-              event.target.style.background = inactiveColor;
-            } else {
-              event.target.style.background = 'white';
-            }
-          }
-        );
-        
-        
         ul.appendChild(li);
       }
     }
-    this.htmlElement.appendChild(ul);
 
+
+    ul.addEventListener(
+      'mouseover',
+      (event) => {
+        //console.log(event.target.className);
+        // console.log(this.game.currentPlayer);
+        // console.log(event.target.tagName);
+        // console.log(event.target.getAttribute('data-pos'));
+        // console.log(event.target.getAttribute('data-pos') instanceof Array);
+        // console.log(event.target.getAttribute('data-pos') instanceof String);
+        // console.log(typeof event.target.getAttribute('data-pos'));
+        // console.log(event.target.getAttribute('data-pos')[0])
+        // let pos = event.target.getAttribute('data-pos');
+        // pos = pos.slice(1, pos.length-1).split(',').map((el) => parseInt(el));
+        // console.log(pos);
+        // event.target.setAttribute("class", `${this.game.currentPlayer}`)
+
+
+        if (event.target.className === '' && event.target.tagName === "LI"){
+          event.target.style.background = 'yellow';
+        } 
+        // setTimeout(() => {
+        //   event.target.style.background = inactiveColor;
+        // },
+        // 500
+        // )
+      }
+    );
+
+    ul.addEventListener(
+      'mouseout',
+      (event) => {
+        if (event.target.tagName === "LI") {
+          if (event.target.className === ''){
+            event.target.style.background = inactiveColor;
+          } else {
+            event.target.style.background = 'white';
+          }
+        }
+      }
+    );
+
+    this.htmlElement.appendChild(ul);
   }
   
-  bindEvents() {}
+  bindEvents() {
 
-  handleClick(e) {}
+  }
+
+  handleClick(e) {
+    if (e.target.className === '' && e.target.tagName === "LI") {
+      let pos = e.target.getAttribute('data-pos');
+      pos = pos.slice(1, pos.length-1).split(',').map((el) => parseInt(el));
+      
+      const currentMark = this.game.currentPlayer;
+      this.game.playMove(pos);
+      e.target.setAttribute("class", `${currentMark}`);
+
+      if (this.game.isOver()) {
+        
+      }
+    }
+  }
 
   makeMove(square) {}
 
