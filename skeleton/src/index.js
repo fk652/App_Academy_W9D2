313cd17087d6cea2572ee15c57f133a9140373ee
Olga <olga.sleepless@gmail.com>
2023-03-07 14:58:31 -0500

    Add board

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require('./ttt-view');// require appropriate file
+const Game = require('../ttt_node/game');// require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const ticTacToeBoard = document.getElementsByClassName('ttt')[0];
+  const view = new View(game, ticTacToeBoard);
 });
--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,7 +1,61 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    console.log(el);
+    this.game = game;
+    this.htmlElement = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const ul = document.createElement('ul');
+    ul.style.display = "flex";
+    ul.style.width = "300px";
+    ul.style.flexWrap = "wrap";
+    ul.style.listStyleType = 'none';
+    ul.style.margin = '30px auto';
+
+    for (let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++){
+        const li = document.createElement('li');
+        li.style.width = '90px';
+        li.style.height = '90px';
+        li.style.border = '1px solid black';
+        const inactiveColor = '#e0e0e0';
+        li.style.background = inactiveColor;
+        li.setAttribute('data-pos',`[${i},${j}]`);
+        li.addEventListener(
+          'mouseover',
+          (event) => {
+            //console.log(event.target.className);
+            if (event.target.className === ''){
+              event.target.style.background = 'yellow';
+            } 
+            // setTimeout(() => {
+            //   event.target.style.background = inactiveColor;
+            // },
+            // 500
+            // )
+          }
+        );
+
+        li.addEventListener(
+          'mouseout',
+          (event) => {
+            if (event.target.className === ''){
+              event.target.style.background = inactiveColor;
+            } else {
+              event.target.style.background = 'white';
+            }
+          }
+        );
+        
+        
+        ul.appendChild(li);
+      }
+    }
+    this.htmlElement.appendChild(ul);
+
+  }
   
   bindEvents() {}
 
